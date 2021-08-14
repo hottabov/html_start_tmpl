5214e530661a8f93a716d6e6b732e7d6c7110757
Vadim <vadim.leonov@mgid.com>
2021-08-14 22:35:16 +0300

    add basic files to tmpl

---
+++ js/_main-append.js
@@ -0,0 +1,4 @@
+//@prepros-append libs/aos.js
+//@prepros-append libs/jquery.twentytwenty.js
+//@prepros-append libs/moment.js
+//@prepros-append script.js
---
+++ js/_script.js
@@ -0,0 +1,7 @@
+// moment.js
+// Чтобы вывести дату, в нужно месте вставить:
+// <script>document.write(moment().subtract(1, 'days').format('DD.MM.YYYY'));</script>
+
+
+
+
