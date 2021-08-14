cc326c602912085218890ea5e5d1740c22f9e0ae
Vadim <vadim.leonov@mgid.com>
2021-08-14 23:48:04 +0300

    add hamburger animation

--- js/_script.js
+++ js/_script.js
@@ -1,7 +1,21 @@
+'use strict';
+
 // moment.js
-// Чтобы вывести дату, в нужно месте вставить:
+// Чтобы вывести дату, в нужном месте вставить:
 // <script>document.write(moment().subtract(1, 'days').format('DD.MM.YYYY'));</script>
 
+window.addEventListener('DOMContentLoaded', () => {
 
+    // Hamburger
+    const iconMenu = document.querySelector(".header__hamburger"),
+        body = document.querySelector("body");
 
+    if (iconMenu) {
+        iconMenu.addEventListener("click", (e) => {
+            iconMenu.classList.toggle("active");
+            body.classList.toggle("lock");
+        });
+    }
+    // END Hamburger
+});
 
